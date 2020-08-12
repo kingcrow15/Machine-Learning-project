766f67fe7b88c82e7e6ecad7585d16425dc0c401
kingcrow15 <61525901+kingcrow15@users.noreply.github.com>
2020-08-11 17:55:27 -0700

    more css adjustments, & api calls fix

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,8 +10,9 @@ d3.csv("../static/data/top_20_movies_and_posters.csv").then(function(top20data) 
   .append("div") // appends a div to placeholder
   .style("height", "500", "width", "357")
   .classed("box col-sm-6 col-md-4 col-lg-3 col-xl-2", true) // sets the class of the new div
+  
   .html(function(d) {
-    return `<img height="500" width="357" src="${d.poster_URL}">`;
+    return `<h3 style="text-align: center"> ${d.movie_name}</h3> <br> <img height="500" width="357" src="${d.poster_URL}">`;
   })
   
   ; // sets the html in the div to an image tag with the link  
