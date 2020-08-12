37473633dadd975bc65b4feea230a22dd3998f1d
kingcrow15 <61525901+kingcrow15@users.noreply.github.com>
2020-08-11 17:30:52 -0700

    loading propper movie posters with nice CSS

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,27 +1,27 @@
 
-
-
-d3.csv("./top_20_movies_and_posters.csv", function(error, data) {
+d3.csv("../static/data/top_20_movies_and_posters.csv").then(function(top20data) {
 // d3.csv("./hours-of-tv-watched.csv").then(function(tvData) {
-    console.log(data);
-}).catch(function(error) {
+    
+  console.log(top20data)
+
+  d3.select(".image_gallery").selectAll("div")
+  .data(top20data)
+  .enter() // creates placeholder for new data
+  .append("div") // appends a div to placeholder
+  .style("height", "500", "width", "357")
+  .classed("box col-sm-6 col-md-4 col-lg-3 col-xl-2", true) // sets the class of the new div
+  .html(function(d) {
+    return `<img height="500" width="357" src="${d.poster_URL}">`;
+  })
+  
+  ; // sets the html in the div to an image tag with the link  
+
+    
+  }).catch(function(error) {
     console.log(error);
   });
 
-// csv1 = "../top_20_movies_and_posters.csv"
 
-// d3.csv(csv1).then(function (data) {
-//   console.log(data)
-// });
-
-// on("load", function (d) {
-//     var g = d3.select(this); // The node
-//             var div = d3.select("body").append("div")
-//                     .attr("class", "mvPoster")
-//                     .img("FIRST LINE <br> SECOND LINE")
-//                     .style("left", (d.x + 50 + "px"))
-//                     .style("top", (d.y +"px"));
-// });
 
 
 
